16d2f8cac39355089951f196490ba8949176133b
Rohan.s <roh142002@gmail.com>
2025-04-29 14:03:33 +0530

    Video5 datatypes completed

---
+++ video5/02_datatypes.js
@@ -0,0 +1,37 @@
+"use strict";           // treat all JS code as newer version 
+
+// alert("Hello World")
+// alert(3+3)          // we are using nodejs, not browser, so alert doesnt work
+
+
+let name = "Rohan.S"    // string
+let age = 23                // number
+let isLoggedIn = true        // boolean
+let isLoggedOut = false             // boolean
+
+let number  = null            // null / to define as empty
+let state;        // undefined
+
+
+// number -> 2 to power 53
+//bigint
+//null  - standalone value / representation of empty value
+//undefined  -  no value assigned
+// symbol - unique
+
+// object
+
+console.log(typeof "Rocky")   // string
+
+console.log(typeof number)  // object
+
+console.log(typeof age)   // number
+
+console.log(typeof state)   // undefined
+
+
+
+
+
+
+
