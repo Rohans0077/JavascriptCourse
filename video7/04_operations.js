c2589e4c58e2caed0212472c7d7d2a3117f76856
Rohan.s <roh142002@gmail.com>
2025-04-30 21:02:48 +0530

    video7 completed

---
+++ video7/04_operations.js
@@ -0,0 +1,25 @@
+let value = 3
+let negativeValue = -value
+console.log(negativeValue);
+
+
+console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2**4)
+console.log(2/3)
+console.log(2%3)
+
+
+let string1 = "Hello"
+let string2 = "Rohan"
+let string3 = string1 + " " + string2         //concatenation of string
+console.log(string3)
+
+
+console.log("1" + 2)         //12
+console.log(1 + "2")    //12
+console.log(1 + "2" + 2)   //122
+console.log(1 + 2 + "2")   //32
+
+
---
+++ video7/05_incrementOperator.js
@@ -0,0 +1,13 @@
+let number1 = 34;
+
+let number2 = number1++
+
+console.log(`The value of number 1 is : ${number1}, and value of number2 is: ${number2}`)
+
+number2 = ++number1
+console.log(`The value of number 1 is : ${number1}, and value of number2 is: ${number2}`)
+
+
+let number3 =  3
+let number4 = number3++
+console.log(`The value of number3 is : ${number3}, and the value of number4 is : ${number4}`)
